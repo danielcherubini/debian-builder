930fe7d38687086148080f045739d18a334f618d
Daniel Cherubini <daniel@cherubini.casa>
2017-02-15 15:44:18 +0100

    added better tests

---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestLoadConfigToModel(t *testing.T) {
+	config, _, err := loadConfigToModel("test.yaml")
+	if err != nil {
+		t.Fail()
+	}
+	if config.Control.Package != "tidal-test-yaml" {
+		t.Fail()
+	}
+}
+
+func TestBuildDebianPackage(t *testing.T) {
+	config, filename, err := loadConfigToModel("test.yaml")
+	if err != nil {
+		t.Fail()
+	}
+	cmd := buildDebianPackage(config.Control, filename)
+	if cmd == nil {
+		t.Fail()
+	}
+}
